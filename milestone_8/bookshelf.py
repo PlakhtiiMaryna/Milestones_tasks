826dea8942ed20ef0c2d193710fad3afa95eb26e
PlakhtiiMaryna <maryna.plakhtii@gmail.com>
2024-11-06 16:12:33 +0200

    Added Class Bookshelf

--- milestone_8/bookshelf.py
+++ milestone_8/bookshelf.py
@@ -1,90 +1,73 @@
 class Book:
     def __init__(self, title_book, author_by_book, category):
-
         self.title_book = title_book
         self.author_by_book = author_by_book
         self.category = category
 
     def __repr__(self):
-        return f'Назва книги: {self.title_book},\nАвтор: {self.author_by_book}'
-    
+        return f'Назва книги: {self.title_book}, Автор: {self.author_by_book}'
+
 book1 = Book('Іди туди, де страшно', 'Джим Лоулесс', 'Популярна психологія')
 book2 = Book('Багатий тато, бідний тато', 'Роберт Кіосакі', 'Бізнес')
 book3 = Book('Майстер і Маргарита', 'Михайло Булгаков', 'Світова класика')
-book4 = Book('Думай і багатій','Наполеон Гілл', 'Бізнес')
-
-# book1
-# book3
-
-
+book4 = Book('Думай і багатій', 'Наполеон Гілл', 'Бізнес')
 
 class Shelf:
-    def __init__(self, category):
-        self.category = category
-        self.books = {} 
-        self.book_counter = 1
+    def __init__(self):
+        self.books = []  
 
     def add_book(self, book):
-        if book.category == self.category:
-            self.books[self.book_counter] = book
-            self.book_counter += 1
-        else:
-            print (f'Книга {book.title_book} не відповідає категорії полиці {self.category}')
+        self.books.append(book)  
 
-    def sort_book(self):
-        self.books = dict(sorted(self.books.items(), key=lambda item: item[1].title_book))
+    def sort_books(self):
+        self.books.sort(key=lambda book: book.title_book)  
 
     def __repr__(self):
-        books_str = "\n".join(f"{num}: {book}" for num, book in self.books.items())
-        return f'Категорія: {self.category},\nКниги: {books_str}'
+        categories = {}  
+        for book in self.books:
+            if book.category not in categories:
+                categories[book.category] = []
+            categories[book.category].append(book)
 
-# business_shelf = Shelf("Бізнес")
-# business_shelf.add_book(book2)
-# business_shelf.add_book(book4)
-# business_shelf.add_book(book3)
-# business_shelf.sort_book()
-# print(business_shelf)
+        result = []
+        for category, books in categories.items():
+            result.append(f"Категорія: {category}:\n" + "\n".join([f"    {i+1}: {book}" for i, book in enumerate(books)]))
+        return "\n\n".join(result)  
+
 
 class Bookshelf:
     def __init__(self):
-        self.book_by_category = {}
+        self.shelves = []  
 
-    def add_shelf(self, category):
-        if category not in self.book_by_category:
-              self.book_by_category[category] = Shelf(category)                    
-        else:
-             print (f'Категорія {category} вже існує') 
+    def add_shelf(self):
+        shelf = Shelf()  
+        self.shelves.append(shelf)  
+        return shelf 
 
-    def add_book_to_shelf(self, book):
-        if book.category in self.book_by_category:
-            self.book_by_category[book.category].add_book(book)
-        else:
-            print (f'Полиця для категорії {book.category} вже існує')
+    def sort_shelves(self):
+        for shelf in self.shelves:
+            shelf.sort_books()  
 
-    def sort_shelf(self):
-        for shelf in self.book_by_category.values():
-            shelf.sort_book()
-    
     def __repr__(self):
-        shelves_str = "\n".join(f"{category}:\n{shelf}" for category, shelf in self.book_by_category.items())
-        return f'Bookshelf:\n{shelves_str}'
+        result = []
+        for i, shelf in enumerate(self.shelves):
+            result.append(f"Полиця {i+1}:\n" + str(shelf))  
+        return "\n\n".join(result)  
 
 
 bookshelf = Bookshelf()
-bookshelf.add_shelf('Популярна психологія')
-bookshelf.add_shelf('Світова класика')
-bookshelf.add_shelf('Бізнес')
+shelf1 = bookshelf.add_shelf()
+shelf1.add_book(book1)
+shelf1.add_book(book2)
+shelf1.add_book(book4)
 
-# Додаємо книги на відповідні полиці
-bookshelf.add_book_to_shelf(book1)
-bookshelf.add_book_to_shelf(book2)
-bookshelf.add_book_to_shelf(book3)
-bookshelf.add_book_to_shelf(book4)
+shelf2 = bookshelf.add_shelf()
+shelf2.add_book(book3)
 
-# Сортуємо книги на полицях
-bookshelf.sort_shelf()
+
+bookshelf.sort_shelves()
+
 
 print(bookshelf)
-
 
 
